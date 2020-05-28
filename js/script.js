89a8f00506104479ff78a275839319bf8956711f
Roman <bliakharr@gmail.com>
2020-05-28 14:07:59 +0300

    landing

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.slider_content').slick({
+    arrow: false,
+    dots: true,
+    adaptiveHeight: true,
+    infinite: false,
+    // variableWidth: true
+    // autoplay: true, автопрокрутка коних 3 сек
+    // centerMode: true, по центру
+  });
+});
